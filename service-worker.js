c3ad540b93a51898fed26f3108269029ed21c391
brynchuk <illia.brynchuk@nixsolutions.com>
2021-07-09 11:13:39 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56475be9cba24000b69e470ba1f5d46c.js"
+  "./precache-manifest.380d5f001bcf4448ce8731e768a54a62.js"
 );
 
 self.addEventListener('message', (event) => {
